67854776c0e74c798f724ca8518815ef8b5c7227
Kohei ASAI <ko2001he@gmail.com>
2025-03-27 22:52:04 +0900

    test jvs

---
+++ tests/test_jvs.py
@@ -0,0 +1,16 @@
+import itertools
+
+import numpy as np
+
+from lhotse_dataset.jvs import JVS
+
+
+class TestJVS:
+    def test_get_cuts(self):
+        corpus = JVS()
+        gen = corpus.get_cuts()
+        for cut in itertools.islice(gen, 3):
+            audio = cut.load_audio()
+            assert isinstance(audio, np.ndarray)
+            assert cut.sampling_rate == 24_000
+            assert cut.duration == audio.shape[1] / cut.sampling_rate
