3d2b29c1a64e3da31849369e44c1380fcdd928d9
Kohei ASAI <ko2001he@gmail.com>
2025-03-27 22:42:50 +0900

    test hifi captain

--- tests/test_hi_fi_captain.py
+++ tests/test_hi_fi_captain.py
@@ -2,15 +2,15 @@ import itertools
 
 import numpy as np
 
-from lhotse_dataset.daily_talk import DailyTalk
+from lhotse_dataset.hi_fi_captain import HiFiCAPTAIN
 
 
-class TestDailyTalk:
+class TestHiFiCAPTAIN:
     def test_get_cuts(self):
-        corpus = DailyTalk()
+        corpus = HiFiCAPTAIN()
         gen = corpus.get_cuts()
         for cut in itertools.islice(gen, 3):
             audio = cut.load_audio()
             assert isinstance(audio, np.ndarray)
-            assert cut.sampling_rate == 44_100
+            assert cut.sampling_rate == 48_000
             assert cut.duration == audio.shape[1] / cut.sampling_rate
