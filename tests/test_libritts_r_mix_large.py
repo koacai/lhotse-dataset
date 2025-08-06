862316d8b13700faa4a7bbf25af79cf6da92941b
Kohei ASAI <ko2001he@gmail.com>
2025-08-06 18:38:35 +0900

    add libritts r mix large

---
+++ tests/test_libritts_r_mix_large.py
@@ -0,0 +1,15 @@
+import itertools
+
+import numpy as np
+
+from lhotse_dataset.libritts_r_mix_large import LibriTTSRMixLarge
+
+
+def test_libritts_r_mix_large() -> None:
+    corpus = LibriTTSRMixLarge()
+    gen = corpus.get_cuts()
+    for cut in itertools.islice(gen, 3):
+        audio = cut.load_audio()
+        assert isinstance(audio, np.ndarray)
+        assert cut.sampling_rate == 24_000
+        assert cut.duration == audio.shape[1] / cut.sampling_rate
