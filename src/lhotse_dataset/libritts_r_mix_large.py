9987181a626fa8c4b6c1fe0ea3ec5abab1c21e46
Kohei ASAI <ko2001he@gmail.com>
2025-08-12 10:56:04 +0900

    fix libri tts r mix large

--- src/lhotse_dataset/libritts_r_mix_large.py
+++ src/lhotse_dataset/libritts_r_mix_large.py
@@ -1,27 +1,34 @@
 import io
-import tarfile
-import tempfile
 import uuid
 from pathlib import Path
 from typing import Generator
 
 import lhotse
 import numpy as np
-import pandas as pd
 import soundfile as sf
+from lhotse import CutSet
+from tqdm import tqdm
 
 from lhotse_dataset.base import BaseCorpus, Language
-from lhotse_dataset.utils import download_file
 
 
 class LibriTTSRMixLarge(BaseCorpus):
+    def __init__(self, libritts_r_shar_dir: str | Path) -> None:
+        super(LibriTTSRMixLarge, self).__init__()
+
+        if isinstance(libritts_r_shar_dir, str):
+            shar_dir = Path(libritts_r_shar_dir)
+        else:
+            shar_dir = libritts_r_shar_dir
+        self.shar_dir = shar_dir
+
     @property
-    def download_url(self) -> dict[str, str]:
+    def subset_samples(self) -> dict[str, int]:
         return {
-            "dev-clean": "https://www.openslr.org/resources/141/dev_clean.tar.gz",
-            "test-clean": "https://www.openslr.org/resources/141/test_clean.tar.gz",
-            "train-clean-100": "https://www.openslr.org/resources/141/train_clean_100.tar.gz",
-            "train-clean-360": "https://www.openslr.org/resources/141/train_clean_360.tar.gz",
+            "test_clean": 5000,
+            "dev_clean": 5000,
+            "train_clean_100": 100000,
+            "train_clean_360": 300000,
         }
 
     @property
@@ -33,140 +40,93 @@ class LibriTTSRMixLarge(BaseCorpus):
         return 1000
 
     def get_cuts(self) -> Generator[lhotse.MultiCut, None, None]:
-        metadata_dir = Path(__file__).parent / "data/libritts_r_mix_large"
-        csv_paths = sorted(list(metadata_dir.glob("*.csv")))
+        cut_paths = sorted(list(map(str, self.shar_dir.glob("cuts.*.jsonl.gz"))))
+        recording_paths = sorted(list(map(str, self.shar_dir.glob("recording.*.tar"))))
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            tmp_dir_path = Path(tmp_dir)
+        cuts = CutSet.from_shar({"cuts": cut_paths, "recording": recording_paths})
+        cuts = cuts.filter(lambda c: c.duration >= 3.0)  # type: ignore
+        cuts = cuts.filter(lambda c: c.duration < 20.0)  # type: ignore
+        cuts = cuts.filter(lambda c: len(c.supervisions) > 0)  # type: ignore
 
-            for csv_path in csv_paths:
-                subset = csv_path.stem
-                df = pd.read_csv(csv_path)
+        for subset, samples in self.subset_samples.items():
+            cuts_subset = cuts.filter(
+                lambda s: s.supervisions[0].custom["subset"] == subset  # type: ignore
+            )
+            cuts_subset = cuts_subset.sort_by_duration()
 
-                tmp_ls_path = tmp_dir_path / f"{subset}.tar.gz"
-                download_file(self.download_url[subset], tmp_ls_path)
+            data_count = 0
+            for cut_1 in tqdm(cuts_subset.data, desc=subset):
+                for cut_2 in cuts_subset.data:
+                    if data_count > samples:
+                        data_count = 0
+                        break
 
-                with tarfile.open(tmp_ls_path) as tar:
-                    tar.extractall(tmp_dir_path, filter="fully_trusted")
+                    if cut_1.duration < cut_2.duration:
+                        continue
+                    if cut_1.supervisions[0].speaker == cut_2.supervisions[0].speaker:
+                        continue
 
-                for row in df.itertuples():
-                    id_1 = row.id_1  # type: ignore
-                    source_1_path = (
-                        tmp_dir_path
-                        / "LibriTTS_R"
-                        / subset
-                        / id_1.split("_")[-4]
-                        / id_1.split("_")[-3]
-                        / f"{'_'.join(id_1.split('_')[-4:])}.wav"
-                    )
-                    id_2 = row.id_2  # type: ignore
-                    source_2_path = (
-                        tmp_dir_path
-                        / "LibriTTS_R"
-                        / subset
-                        / id_2.split("_")[-4]
-                        / id_2.split("_")[-3]
-                        / f"{'_'.join(id_2.split('_')[-4:])}.wav"
-                    )
+                    i = np.random.uniform(0, 1)
+                    if i < 0.001 and data_count <= samples:
+                        wav_1 = cut_1.load_audio()
+                        wav_2 = cut_2.load_audio()
+                        wav_len = max(wav_1.shape[-1], wav_2.shape[-1])
 
-                    wav_1, sr = sf.read(source_1_path)
-                    wav_2, sr = sf.read(source_2_path)
-                    wav_len = max(wav_1.shape[0], wav_2.shape[0])
+                        wav = np.zeros((2, wav_len), dtype=wav_1.dtype)
+                        wav[0, : wav_1.shape[-1]] = wav_1
+                        wav[1, : wav_2.shape[-1]] = wav_2
 
-                    wav = np.zeros((2, wav_len), dtype=wav_1.dtype)
-                    wav[0, : wav_1.shape[0]] = wav_1
-                    wav[1, : wav_2.shape[0]] = wav_2
+                        buf = io.BytesIO()
+                        sr = cut_1.sampling_rate
+                        sf.write(buf, wav.T, sr, format="WAV")
 
-                    buf = io.BytesIO()
-                    sf.write(buf, wav.T, sr, format="WAV")
-
-                    mixture_id = uuid.uuid4().hex
-                    recording = lhotse.Recording.from_bytes(
-                        buf.getvalue(), recording_id=f"recording_{mixture_id}"
-                    )
-                    assert recording.channel_ids is not None
-
-                    normalized_txt_path_1 = (
-                        source_1_path.parent / f"{source_1_path.stem}.normalized.txt"
-                    )
-                    try:
-                        with open(normalized_txt_path_1, "r", encoding="utf-8") as f:
-                            normalized_txt_1 = f.readline()
-                    except FileNotFoundError:
-                        print(
-                            "Warning: Normalized text file not found for", source_1_path
+                        mixture_id = uuid.uuid4().hex
+                        recording = lhotse.Recording.from_bytes(
+                            buf.getvalue(), recording_id=f"recording_{mixture_id}"
                         )
-                        normalized_txt_1 = ""
+                        assert recording.channel_ids is not None
 
-                    original_txt_path_1 = (
-                        source_1_path.parent / f"{source_1_path.stem}.original.txt"
-                    )
-                    try:
-                        with open(original_txt_path_1, "r", encoding="utf-8") as f:
-                            original_txt_1 = f.readline()
-                    except FileNotFoundError:
-                        print(
-                            "Warning: Original text file not found for", source_1_path
+                        s1 = cut_1.supervisions[0]
+                        assert s1.custom is not None
+                        supervision_source_1 = lhotse.SupervisionSegment(
+                            id=s1.id,
+                            recording_id=recording.id,
+                            start=0,
+                            duration=wav_1.shape[-1] / sr,
+                            channel=0,
+                            text=s1.text,
+                            custom={
+                                "wav_len": wav_1.shape[-1],
+                                "original_text": s1.custom["original_text"],
+                            },
                         )
-                        original_txt_1 = ""
-
-                    normalized_txt_path_2 = (
-                        source_2_path.parent / f"{source_2_path.stem}.normalized.txt"
-                    )
-                    try:
-                        with open(normalized_txt_path_2, "r", encoding="utf-8") as f:
-                            normalized_txt_2 = f.readline()
-                    except FileNotFoundError:
-                        print(
-                            "Warning: Normalized text file not found for", source_2_path
+                        s2 = cut_2.supervisions[0]
+                        assert s2.custom is not None
+                        supervision_source_2 = lhotse.SupervisionSegment(
+                            id=s2.id,
+                            recording_id=recording.id,
+                            start=0,
+                            duration=wav_2.shape[-1] / sr,
+                            channel=1,
+                            text=s2.text,
+                            custom={
+                                "wav_len": wav_2.shape[-1],
+                                "original_text": s2.custom["original_text"],
+                            },
                         )
-                        normalized_txt_2 = ""
 
-                    original_txt_path_2 = (
-                        source_2_path.parent / f"{source_2_path.stem}.original.txt"
-                    )
-                    try:
-                        with open(original_txt_path_2, "r", encoding="utf-8") as f:
-                            original_txt_2 = f.readline()
-                    except FileNotFoundError:
-                        print(
-                            "Warning: Original text file not found for", source_2_path
+                        cut = lhotse.MultiCut(
+                            id=mixture_id,
+                            start=0,
+                            duration=recording.duration,
+                            supervisions=[supervision_source_1, supervision_source_2],
+                            channel=recording.channel_ids,
+                            recording=recording,
+                            custom={"subset": subset},
                         )
-                        original_txt_2 = ""
+                        data_count += 1
+                        yield cut
 
-                    supervision_source_1 = lhotse.SupervisionSegment(
-                        id=f"source_1_{source_1_path}",
-                        recording_id=recording.id,
-                        start=0,
-                        duration=wav_1.shape[0] / sr,
-                        channel=0,
-                        text=normalized_txt_1,
-                        custom={
-                            "wav_len": wav_1.shape[0],
-                            "original_txt": original_txt_1,
-                        },
-                    )
-                    supervision_source_2 = lhotse.SupervisionSegment(
-                        id=f"source_2_{source_2_path}",
-                        recording_id=recording.id,
-                        start=0,
-                        duration=wav_2.shape[0] / sr,
-                        channel=1,
-                        text=normalized_txt_2,
-                        custom={
-                            "wav_len": wav_2.shape[0],
-                            "original_txt": original_txt_2,
-                        },
-                    )
-
-                    cut = lhotse.MultiCut(
-                        id=mixture_id,
-                        start=0,
-                        duration=recording.duration,
-                        supervisions=[supervision_source_1, supervision_source_2],
-                        channel=recording.channel_ids,
-                        recording=recording,
-                        custom={"subset": subset},
-                    )
-
-                    yield cut
+                else:
+                    continue
+                break
